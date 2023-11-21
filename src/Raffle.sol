a614ba92dd3eb16e5ef1f75b1a8342c18b3f5533
Parth <devparthmandale915@gmail.com>
2023-11-22 01:19:05 +0530

    tests

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -21,39 +21,35 @@
 
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.19;
 
 import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import {AutomationCompatibleInterface} from "@chainlink/contracts/src/v0.8/interfaces/AutomationCompatibleInterface.sol";
 
 /**@title A sample Raffle Contract
- * @author Parth
+ * @author Patrick Collins
  * @notice This contract is for creating a sample raffle contract
  * @dev This implements the Chainlink VRF Version 2
  */
-
 contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
-    // Errors
-    // Custom errors
-
-    error Raffle__SendMoreToEnterRaffle();
-    error Raffle__RaffleIsNotOpen();
-    error Raffle__TransferFailed();
+    /* Errors */
     error Raffle__UpkeepNotNeeded(
         uint256 currentBalance,
         uint256 numPlayers,
         uint256 raffleState
     );
+    error Raffle__TransferFailed();
+    error Raffle__SendMoreToEnterRaffle();
+    error Raffle__RaffleNotOpen();
 
+    /* Type declarations */
     enum RaffleState {
         OPEN,
         CALCULATING
     }
-
     /* State variables */
     // Chainlink VRF Variables
-
     VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
     uint64 private immutable i_subscriptionId;
     bytes32 private immutable i_gasLane;
@@ -61,8 +57,7 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
     uint32 private constant NUM_WORDS = 1;
 
-    //Lottery var
-
+    // Lottery Variables
     uint256 private immutable i_interval;
     uint256 private immutable i_entranceFee;
     uint256 private s_lastTimeStamp;
@@ -70,6 +65,12 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
     address payable[] private s_players;
     RaffleState private s_raffleState;
 
+    /* Events */
+    event RequestedRaffleWinner(uint256 indexed requestId);
+    event RaffleEnter(address indexed player);
+    event WinnerPicked(address indexed player);
+
+    /* Functions */
     constructor(
         uint64 subscriptionId,
         bytes32 gasLane, // keyHash
@@ -88,22 +89,15 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
         i_callbackGasLimit = callbackGasLimit;
     }
 
-    /* Events */
-    event RequestedRaffleWinner(uint256 indexed requestId);
-    event RaffleEnter(address indexed player);
-    event WinnerPicked(address indexed player);
-
     function enterRaffle() public payable {
         // require(msg.value >= i_entranceFee, "Not enough value sent");
         // require(s_raffleState == RaffleState.OPEN, "Raffle is not open");
         if (msg.value < i_entranceFee) {
             revert Raffle__SendMoreToEnterRaffle();
         }
-
         if (s_raffleState != RaffleState.OPEN) {
-            revert Raffle__RaffleIsNotOpen();
+            revert Raffle__RaffleNotOpen();
         }
-
         s_players.push(payable(msg.sender));
         // Emit an event when we update a dynamic array or mapping
         // Named events with the function name reversed
@@ -119,8 +113,6 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
      * 3. The contract has ETH.
      * 4. Implicity, your subscription is funded with LINK.
      */
-
-    // if this is true then perform upkeep is called
     function checkUpkeep(
         bytes memory /* checkData */
     )
@@ -163,31 +155,14 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
         emit RequestedRaffleWinner(requestId);
     }
 
-    // Automation here is important
-    function pickWinner(uint256 /* requestId */) external {
-        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
-            revert();
-        }
-
-        s_raffleState = RaffleState.CALCULATING;
-        uint256 requestId = i_vrfCoordinator.requestRandomWords(
-            i_gasLane,
-            i_subscriptionId,
-            REQUEST_CONFIRMATIONS,
-            i_callbackGasLimit,
-            NUM_WORDS
-        );
-        // Quiz... is this redundant?
-        emit RequestedRaffleWinner(requestId);
-    }
-
+    /**
+     * @dev This is the function that Chainlink VRF node
+     * calls to send the money to the random winner.
+     */
     function fulfillRandomWords(
         uint256 /* requestId */,
         uint256[] memory randomWords
     ) internal override {
-        // CHECKS, EFFECTS, INTERACTONS
-
-        // Easy random logic
         // s_players size 10
         // randomNumber 202
         // 202 % 10 ? what's doesn't divide evenly into 202?
